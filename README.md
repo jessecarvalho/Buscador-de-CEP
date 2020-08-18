3f81315e0ea013affe190ee107836f6035734abc
jesseCarvalho <jesseelias80@gmail.com>
2020-08-18 16:03:07 -0300

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # Buscador de CEP
+## O projeto 
+O projeto foi desenvolvido com o objetivo de possibilitar ao usuário fazer uma pesquisa por CEP da maneira mais rápida
+e simples possível.
+## O que foi usado
+- Javascript
+- Node.JS
+- CSS
+- HTML
+- Boostrap
+## Créditos
+- Favicon: por ceder gratuítamente o ícon do site.
+- MasashiWakui: por ceder gratuítamente via pixabay a foto de background do site.
+
  
--- app/public/src/controller/SiteController.js
+++ app/public/src/controller/SiteController.js
@@ -1,7 +1,9 @@
 class SiteController {
 
+    //Método construtor
     constructor() {
 
+        /*Capturando elementos*/
         this.btnSearchEl = document.querySelector("#btn-search");
         this.errorEl = document.querySelector("#error");
         this.itensSearchEl = document.querySelector(".items-search");
@@ -10,64 +12,70 @@ class SiteController {
 
     }
 
+    //Inicialização de eventos
     initEvents(){
 
+        //Relacionando uma atividade ao botão de search
         this.btnSearchEl.addEventListener('click', evt => {
-
+            //Impedindo o refresh do site
             evt.preventDefault();
-
+            //Capturando o valor digitado no input
             let cep = document.querySelector("#input-cep");
-
+            //Chamando a função que busca o cep
             this.searchCep(cep.value);
-
         });
 
+        //Relacionando uma atividade ao botão de voltar
         document.querySelectorAll(".btn-back").forEach(btn =>
             btn.addEventListener('click', evt => {
+                //Aqui será ocultado os resultados passando a mostrar apenas o fomulário de search
                 this.displayItemsEl.style.display = "none";
                 this.errorEl.style.display = "none";
                 this.itensSearchEl.style.display = "block";
-
             })
         );
-
-
-
     }
 
+    //Função de busca, recebe o CEP via parâmetro
     searchCep(cep){
+        //substitui o traço do cep caso tenha sido digitado por vázio
         cep = cep.replace('-', '')
-
+        //Caso o cep seja inexistente, retornar um alert.
         if (cep === ""){
-            return false;
+            alert("Digite um cep, por favor");
+            return;
         }
 
+        //Promisse para realizar a busca via API
         fetch("https://cors-anywhere.herokuapp.com/viacep.com.br/ws/"+ cep + "/json")
             .then(response => response.json())
             .then(data => {
+                //Salvando os valores recebidos via API em variáveis temporárias
                 let streetName = data['logradouro'];
                 let neighborhood = data['bairro'];
                 let city = data['localidade'];
                 let state = data['uf'];
 
-                if (streetName === undefined){
+                //Caso a API tenha retornado um valor indefinido, forçar um erro
+                if (streetName === undefined || neighborhood === undefined || city === undefined || state === undefined){
                     throw new Error();
                 }
 
+                //Caso tenha passado por todas verificações com sucesso, inserir no html o resultado obtido
                 document.querySelector("#street").innerHTML = "Localidade: " + streetName;
                 document.querySelector("#neighborhood").innerHTML = "Bairro: " + neighborhood;
                 document.querySelector("#city").innerHTML = "Cidade: " + city;
                 document.querySelector("#state").innerHTML = "Estado: " + state;
+
+                //Ocultar o fomulário de busca e apresentar a resposta ao usuário
                 this.itensSearchEl.style.display = "none";
                 this.displayItemsEl.style.display = "block";
             })
             .catch(err => {
+                //Caso tenha caído em alguma das verificações apresente um erro na tela para o usuário.
                 document.querySelector("#error-item").innerHTML = "Cep incorreto!";
                 this.itensSearchEl.style.display = "none";
                 this.errorEl.style.display = "block";
             })
-
-
     }
-
 }
